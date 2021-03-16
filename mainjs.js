06a72a8e229fabfeffcc46587301213c1b053f24
medsaiefbardi <bardimohamedsaief@gmail.com>
2021-03-16 13:40:30 +0100

    init

---
+++ mainjs.js
@@ -0,0 +1,22 @@
+var paragraphs=[
+{pp1:'Keep youself ',
+                  w1 :'away ',
+                  pp2:'from negative people so that will not affect ',
+                  w2 : 'your vibe ',
+                  pp3 : 'and always remember ',
+                  w3: 'CARPE DIEM!'}   ,
+                  
+{pp1:'Focus on your ',
+                  w1 :'goals ',
+                  pp2:'.It does not matter how hard it is ',
+                  w2 : 'Just do it BROOO , ',
+                  pp3 : 'and always remember ',
+                  w3: 'CARPE DIEM!'} ,
+
+ {pp1:'Take it ',
+                  w1 :'easy ',
+                  pp2:' .Do not overestimate problems ',
+                  w2 : 'Try to solve it, ',
+                  pp3 : 'and always remember ',
+                  w3: 'CARPE DIEM!'} 
+]
